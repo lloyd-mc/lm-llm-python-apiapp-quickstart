88f4f51806c8e2c0e58b42dd158264f6973f6047
Lloyd McKie <lloyd.mckie+sourcetree@gmail.com>
2023-07-23 12:43:08 +0800

    ci: frontend with proper mapping

--- frontend/up.sh
+++ frontend/up.sh
@@ -1,3 +1,13 @@
 docker compose up -d
-
+# https://upcloud.com/resources/tutorials/deploy-wordpress-with-docker-compose
 start http://localhost:8080
+
+start http://localhost:8080/wp-admin/setup-config.php?step=1
+
+
+# admin
+start http://localhost:8080/wp-admin/install.php
+
+
+# admin
+start http://localhost:8080/wp-admin/install.php
